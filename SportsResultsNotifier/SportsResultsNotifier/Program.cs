9c4affd9d226e1756c82ae362f5a7b90d1b58b7f
Rody10 <rodneysimbatholanah@gmail.com>
2025-02-26 18:18:47 +0200

    second commit

---
+++ SportsResultsNotifier/SportsResultsNotifier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsResultsNotifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
