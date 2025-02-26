01482f5a99252ea8b4cb74c7398c2ca9e0cbc9f8
Rody10 <rodneysimbatholanah@gmail.com>
2025-02-26 20:09:50 +0200

    implemented scrapping of results as text in chaotic form

--- SportsResultsNotifier/SportsResultsNotifier/Program.cs
+++ SportsResultsNotifier/SportsResultsNotifier/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace SportsResultsNotifier
+﻿using HtmlAgilityPack;
+
+namespace SportsResultsNotifier
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sports Results Notifier");
+            var html = @"https://www.basketball-reference.com/boxscores/";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+
+            // Get collection of nodes that contain results.
+            var resultsNodes = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div[*]/table[1]/tbody");
+            
+            foreach(var node in resultsNodes)
+            {
+                Console.WriteLine(node.InnerText);
+            }
+
+            
+
+ 
         }
     }
 }
